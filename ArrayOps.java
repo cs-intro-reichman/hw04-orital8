dec0019625daf09d2f13ce6305da02487b6488a7
orital8 <ori.tal01@post.runi.ac.il>
2024-01-11 08:02:06 +0200

    dd

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,89 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+     int [] array = {8, 7 ,3,3};
+     int [] array1 = {0,1,2,8,};
+     System.out.println(isSorted(array));
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int length = (array.length);
+        int [] arraycheck = new int[length+1];
+        int missing = 1;
+        for(int i=0 ; i<=length-1 ; i++){
+            for(int j = 0 ; j <= length ; j++) {
+        if (array[i] == j) 
+        arraycheck[j]=j;
+        else;
+        }
+    }
+     for(int m=0 ; m<=length ; m++){
+        if  (arraycheck[m] != m )
+        missing=m;
+        else;
+    }        
+        return missing;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int max = array[0];
+        int secmax = array[0];
+          for(int i=0 ; i <= array.length-1 ; i++){
+          if ((array[i] > secmax) && (array[i] > max )) {
+            secmax = max;
+            max = array [i];
+          }
+            else;
+            if (array[i] > secmax && array[i] < max ) 
+            secmax = array [i];
+            else;
+        }
+        return secmax;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        boolean check = true;
+         for(int i = 0 ; i < array1.length ; i++){
+             check =false;
+            int a = array1[i];
+            for(int j = 0 ; j < array2.length ; j++) {
+                int b = array2 [j] ;
+                if (a == b)
+                check = true;
+                else;
+            }
+            if (check == false)
+            return check;
+        }
+        return check;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        boolean check = true;
+        int a = array[0];
+        int b = array[1];
+        if (a > b){
+          for(int i = 0 ; i < array.length-1 ; i++){
+            a = array[i];
+            b = array[i+1];
+            if (b >= a){
+                check = false;
+                return check;
+            }
+            else;
+                  }
+        }
+        if (b > a){
+             for(int i = 0 ; i < array.length-1 ; i++){
+              a = array[i];
+            b = array[i+1];
+            if (a >= b){
+                check = false;
+                return check;
+            }
+            else;
+          }
+        }
+            return check;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -22,12 +22,30 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        String string = "Hello my FRienD mY nAme is";
+     System.out.println(capVowelsLowRest(string));
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String str = "";
+        String str1 ="";
+        char c;
+        for (int i = 0 ; i < string.length(); i++){
+            c = string.charAt(i);
+			if ( 90 >= c && c >= 65)
+			c=(char) (c+32);
+            str = str +c ;
+        }
+         for (int j = 0 ; j < str.length(); j++){
+             c = str.charAt(j);
+			if ( c == 97 || c == 101 || c == 105 || c ==111 || c == 117){
+			c=(char) (c - 32);
+            str1 = str1 +c ;
+         }
+         else 
+         str1 = str1 +c ;
+         }
+        return str1;
     }
 
     public static String camelCase (String string) {
